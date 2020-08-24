47357cc29e77e701061699dc4d67526444d073a4
Thomazoide <t.cannobbioabarca@uandresbello.edu>
2020-08-24 17:18:38 -0400

    Objeto persona

--- main.py
+++ main.py
@@ -14,6 +14,26 @@ class Persona():
         if(self.sex=='m' or self.sex=='M'):
             if(self.imc<20):
                 self.desc_imc='BAJO PESO'
-            elif(self.imc>=20 and self.imc<24.9):
+            elif(self.imc>=20 and self.imc<=24.9):
                 self.desc_imc='NORMAL'
-            
+            elif(self.imc>=25 and self.imc<=29.9):
+                self.desc_imc='OBESIDAD LEVE'
+            elif(self.imc>=30 and self.imc<=40):
+                self.desc_imc='OBESIDAD SEVERA'
+            elif(self.imc>40):
+                self.desc_imc='OBESIDAD MUY SEVERA'
+            else:
+                self.desc_imc='caso particular...'
+        else:
+            if(self.imc<20):
+                self.desc_imc='BAJO PESO'
+            elif(self.imc>=20 and self.imc<=23.9):
+                self.desc_imc='NORMAL'
+            elif(self.imc>=24 and self.imc<=28.9):
+                self.desc_imc='OBESIDAD LEVE'
+            elif(self.imc>=29 and self.imc<=37):
+                self.desc_imc='OBESIDAD SEVERA'
+            elif(self.imc>37):
+                self.desc_imc='OBESIDAD MUY SEVERA'
+            else:
+                self.desc_imc='caso particular...'
