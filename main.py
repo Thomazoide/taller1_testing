0aa8a78d595e585a3d347491b3faf14977a1dff1
Thomazoide <t.cannobbioabarca@uandresbello.edu>
2020-08-28 19:21:15 -0400

    casi terminado

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time, os
+import datetime, os
 class Persona():
     def __init__(self, nombre, rut, sexo, fnacimiento, peso, fechapesaje, altura, aon):
         self.nom=nombre
@@ -10,6 +10,7 @@ class Persona():
         self.fechapesaje=fechapesaje
         self.altura=altura
         self.desc_imc=''
+        self.desc_gc=''
     @property
     def obtenerIMC(self):
         self.imc=self.peso/self.altura**2
@@ -39,6 +40,146 @@ class Persona():
                 self.desc_imc='OBESIDAD MUY SEVERA'
             else:
                 self.desc_imc='caso particular...'
+    @property
+    def obtenerGC(self):
+        e=self.fecha.split('/')
+        n=datetime.datetime.now()
+        self.edad=int(n.year)-int(e[-1])
+        self.gc=int
+        if(self.sex=='m' or self.sex=='M'):
+            self.gc=((1.2*self.imc)+(0.23*self.edad-10.8-5.4))
+            if(self.edad>=19 and self.edad<=24):
+                if(self.gc>=10.8 and self.gc<=14.9):
+                    self.desc_gc='Optimo'
+                elif(self.gc>15 and self.gc<=23.3):
+                    self.desc_gc='Bueno'
+                elif(self.gc>23.3):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=25 and self.edad<=29):
+                if(self.gc>=12.8 and self.gc<=16.5):
+                    self.desc_gc='Optimo'
+                elif(self.gc>16.5 and self.gc<=24.4):
+                    self.desc_gc='Bueno'
+                elif(self.gc>24.4):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=30 and self.edad<=34):
+                if(self.gc>=14.5 and self.gc<=18):
+                    self.desc_gc='Optimo'
+                elif(self.gc>18 and self.gc<=25.2):
+                    self.desc_gc='Bueno'
+                elif(self.gc>25.2):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=35 and self.edad<=39):
+                if(self.gc>=16.1 and self.gc<=19.4):
+                    self.desc_gc='Optimo'
+                elif(self.gc>19.4 and self.gc<=26.1):
+                    self.desc_gc='Bueno'
+                elif(self.gc>26.1):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=40 and self.edad<44):
+                if(self.gc>=17.5 and self.gc<=20.5):
+                    self.desc_gc='Optimo'
+                elif(self.gc>20.5 and self.gc<=26.9):
+                    self.desc_gc='Bueno'
+                elif(self.gc>26.9):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=45 and self.edad<=49):
+                if(self.gc>=18.6 and self.gc<=21.5):
+                    self.desc_gc='Optimo'
+                elif(self.gc>21.5 and self.gc<=27.6):
+                    self.desc_gc='Bueno'
+                elif(self.gc>27.6):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=50 and self.edad<=54):
+                if(self.gc>=19.8 and self.gc<=22.7):
+                    self.desc_gc='Optimo'
+                elif(self.gc>22.7 and self.gc<=28.7):
+                    self.desc_gc='Bueno'
+                elif(self.gc>28.7):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=55 and self.edad<=59):
+                if(self.gc>=20.2 and self.gc<=23.2):
+                    self.desc_gc='Optimo'
+                elif(self.gc>23.2 and self.gc<=29.3):
+                    self.desc_gc='Bueno'
+                elif(self.gc>29.3):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=60):
+                if(self.gc>=20.3 and self.gc<=23.5):
+                    self.desc_gc='Optimo'
+                elif(self.gc>23.5 and self.gc<=29.8):
+                    self.desc_gc='Bueno'
+                elif(self.gc>29.8):
+                    self.desc_gc='Malo(Obesidad)'
+            else:
+                self.desc_gc='Caso particular...'
+        else:
+            self.gc=((1.2*self.imc)+0.23*self.edad-5.4)
+            if(self.edad>=19 and self.edad<=24):
+                if(self.gc>=18.9 and self.gc<=22.1):
+                    self.desc_gc='Optimo'
+                elif(self.gc>22.1 and self.gc<=29.6):
+                    self.desc_gc='Bueno'
+                elif(self.gc>29.6):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=25 and self.edad<=29):
+                if(self.gc>=18.9 and self.gc<=22):
+                    self.desc_gc='Optimo'
+                elif(self.gc>22 and self.gc<=29.8):
+                    self.desc_gc='Bueno'
+                elif(self.gc>29.8):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=30 and self.edad<=34):
+                if(self.gc>=19.7 and self.gc<=22.7):
+                    self.desc_gc='Optimo'
+                elif(self.gc>22.7 and self.gc<=30.5):
+                    self.desc_gc='Bueno'
+                elif(self.gc>30.5):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=35 and self.edad<=39):
+                if(self.gc>=21 and self.gc<=24):
+                    self.desc_gc='Optimo'
+                elif(self.gc>24 and self.gc<=31.5):
+                    self.desc_gc='Bueno'
+                elif(self.gc>31.5):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=40 and self.edad<=44):
+                if(self.gc>=22.6 and self.gc<=25.6):
+                    self.desc_gc='Optimo'
+                elif(self.gc>25.5 and self.gc<=32.8):
+                    self.desc_gc='Bueno'
+                elif(self.gc>32.8):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=45 and self.edad<=49):
+                if(self.gc>=24.3 and self.gc<=27.3):
+                    self.desc_gc='Optimo'
+                elif(self.gc>27.3 and self.gc<=34.1):
+                    self.desc_gc='Bueno'
+                elif(self.gc>34.1):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=50 and self.edad<=54):
+                if(self.gc>=26.6 and self.gc<=29.7):
+                    self.desc_gc='Optimo'
+                elif(self.gc>29.7 and self.gc<=36.2):
+                    self.desc_gc='Bueno'
+                elif(self.gc>36.2):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=55 and self.edad<=59):
+                if(self.gc>=27.4 and self.gc<=30.7):
+                    self.desc_gc='Optimo'
+                elif(self.gc>30.7 and self.gc<=37.3):
+                    self.desc_gc='Bueno'
+                elif(self.gc>37.3):
+                    self.desc_gc='Malo(Obesidad)'
+            elif(self.edad>=60):
+                if(self.gc>=27.6 and self.gc<=31):
+                    self.desc_gc='Optimo'
+                elif(self.gc>31 and self.gc<=38):
+                    self.desc_gc='Bueno'
+                elif(self.gc>38):
+                    self.desc_gc='Malo(Obesidad)'
+            else:
+                self.desc_gc='Caso particular...'
 def clear():
     if os.name=="nt":
         os.system("cls")
@@ -65,6 +206,7 @@ if __name__=='__main__':
             if len(personas)!=0:
                 for i in personas:
                     i.obtenerIMC
+                    i.obtenerGC
             print('Agregado/a correctamente...')
             input('Presione enter para continuar...')
             clear()
@@ -72,7 +214,7 @@ if __name__=='__main__':
             rut=input('Ingrese rut: ')
             for i in personas:
                 if i.rut==rut:
-                    print('Persona encontrada!\nNombre: ', i.nom,'\nRut: ',i.rut,'\nSexo: ', i.sex, '\nFecha de nacimiento: ', i.fecha)
+                    print('Persona encontrada!\nNombre: ', i.nom,'\nRut: ',i.rut,'\nSexo: ', i.sex, '\nFecha de nacimiento: ', i.fecha,'\nIMC: ', i.imc)
                     print('Peso: ', i.peso,'\nFecha de pesaje: ',i.fechapesaje,'\nAltura: ',i.altura,' mts.\nAtleta: ', i.aon, '\nEstado: ', i.desc_imc)
                 else:
                     print('Persona no registrada...')
